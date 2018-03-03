3d1386209dba2a638a5c2df4e4f83eb4e6ed5d8d
Afshar Mohebbi <afshar.mohebbi@gmail.com>
2018-03-03 10:46:01 +0330

    Tested using ConcurrencyCheck attribute of EF

--- ConcurrencyTest/Models/City.cs
+++ ConcurrencyTest/Models/City.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,9 @@ namespace ConcurrencyTest.Models
         public int ID { get; set; }
 
         public string Name { get; set; }
+
+        //Putting [ConcurrencyCheck] attribute here on the Number field, prevents concurrency update problem. This is done autmatically by EF
+        //For more info see https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/handling-concurrency-with-the-entity-framework-in-an-asp-net-mvc-application
         public int Number { get; set; }
     }
 }
